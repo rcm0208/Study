115afdb29859f5af9498fe1b2cd9563f373b8d59
rcm0208 <c_m__00@outlook.jp>
2024-03-20 15:55:31 +0900

    section5-43

--- react-guide-material/05_state_and_event/src/010_eventlistener/start/Example.js
+++ react-guide-material/05_state_and_event/src/010_eventlistener/start/Example.js
@@ -1,6 +1,21 @@
 const Example = () => {
+  const clickHandler = () => {
+    alert('ボタンがクリックされました。');
+  };
+  const clickHandler2 = () => {
+    console.log('ボタンがクリックされました。');
+  };
+
+  const hello = () => {
+    return 'hello react';
+  };
+  console.log(hello());
   return (
-    <></>
+    <>
+      <button onClick={clickHandler}>クリックしてね</button>
+      <button onClick={clickHandler2}>クリックしてね</button>
+      {hello}
+    </>
   );
 };
 
